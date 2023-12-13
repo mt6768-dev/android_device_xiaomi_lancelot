bfb0b93fe1aa2c81f2767f8529d3d331b980e6c8
Matsvei Niaverau <matvej2002@gmail.com>
2023-12-13 13:26:16 +0100

    lancelot: Update from `V13.0.6.0.SJCMIXM`
    Change-Id: I65a815bbf839d19612d7b6687cc81e8ee0e1ecd8

--- lineage_lancelot.mk
+++ lineage_lancelot.mk
@@ -23,6 +23,6 @@ PRODUCT_MODEL := Redmi 9
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="lancelot-user 11 RP1A.200720.011 V12.5.6.0.RJCMIXM release-keys"
+    PRIVATE_BUILD_DESC="lancelot-user 12 SP1A.210812.016 V13.0.4.0.SJCMIXM release-keys"
 
-BUILD_FINGERPRINT := Redmi/lancelot_global/lancelot:11/RP1A.200720.011/V12.5.6.0.RJCMIXM:user/release-keys
+BUILD_FINGERPRINT := Redmi/lancelot_global/lancelot:12/SP1A.210812.016/V13.0.4.0.SJCMIXM:user/release-keys
