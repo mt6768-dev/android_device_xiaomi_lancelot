c28720294e056edc08dbbfc8a0cac7826f6a0427
Bruno Martins <bgcngm@gmail.com>
2026-07-13 12:37:44 +0200

    lancelot: Add firmware extraction support
    Rename from factory image dump to the common partition naming.
    
    Change-Id: I61b0f4cdddf65166cb12a39926e1fcb504241975

--- extract-files.py
+++ extract-files.py
@@ -29,7 +29,7 @@ module = ExtractUtilsModule(
     'xiaomi',
     blob_fixups=blob_fixups,
     namespace_imports=namespace_imports,
-    add_firmware_proprietary_file=False,
+    add_firmware_proprietary_file=True,
 )
 
 if __name__ == '__main__':
