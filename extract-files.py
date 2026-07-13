7a3c4ab53feebea3203be777b0c74c6b4a70316b
Matsvei Niaverau <matvej2002@gmail.com>
2026-07-13 13:48:34 +0200

    lancelot: Decommonize audio blob patching
    Change-Id: I0c096f044ac8ed2669407691b6d7e936a470f74a

--- extract-files.py
+++ extract-files.py
@@ -22,6 +22,8 @@ namespace_imports = [
 blob_fixups: blob_fixups_user_type = {
     'vendor/bin/mnld': blob_fixup()
         .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so'),
+    'vendor/lib/hw/audio.primary.mt6768.so': blob_fixup()
+        .replace_needed('libalsautils.so', 'libalsautils-v31.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
