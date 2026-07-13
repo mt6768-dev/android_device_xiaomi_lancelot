211ba01ae03c34b2562036cc7f71559cef8bc499
bengris32 <bengris32@protonmail.ch>
2026-07-13 13:53:07 +0200

    lancelot: Link Audio HAL against V33 libstagefright_foundation
    Change-Id: Id2dfd90a671846fb0108503d642195eaa4a25f12
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- extract-files.py
+++ extract-files.py
@@ -23,6 +23,7 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/bin/mnld': blob_fixup()
         .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so'),
     'vendor/lib/hw/audio.primary.mt6768.so': blob_fixup()
+        .add_needed('libstagefright_foundation-v33.so')
         .replace_needed('libalsautils.so', 'libalsautils-v31.so')
         .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
     'vendor/lib/librt_extamp_intf.so' : blob_fixup()
