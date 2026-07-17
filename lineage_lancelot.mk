58b5adb4716bb6374b9c5c867f61cdac55bd6350
LuK1337 <priv.luk@gmail.com>
2026-07-17 13:10:50 +0200

    lancelot: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- lineage_lancelot.mk
+++ lineage_lancelot.mk
@@ -23,6 +23,9 @@ PRODUCT_MODEL := Redmi 9
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="lancelot-user 12 SP1A.210812.016 V13.0.4.0.SJCMIXM release-keys"
-
-BUILD_FINGERPRINT := Redmi/lancelot_global/lancelot:12/SP1A.210812.016/V13.0.4.0.SJCMIXM:user/release-keys
+    BuildDesc="lancelot-user 12 SP1A.210812.016 V13.0.4.0.SJCMIXM release-keys" \
+    BuildFingerprint=Redmi/lancelot_global/lancelot:12/SP1A.210812.016/V13.0.4.0.SJCMIXM:user/release-keys \
+    SystemModel=$(PRODUCT_SYSTEM_DEVICE) \
+    SystemName=$(PRODUCT_SYSTEM_NAME) \
+    ProductModel=$(PRODUCT_SYSTEM_DEVICE) \
+    DeviceProduct=$(PRODUCT_SYSTEM_NAME)
