3e6fe78e90a411e6e16bf048c0437da162867fa9
Matsvei Niaverau <matvej2002@gmail.com>
2026-07-31 17:02:15 +0200

    lancelot: Set `ro.product.marketname` property
    - Orange users like me have RIL crashes without that
    
    Change-Id: Ib82282188ad163022d2c9ef3c986b18bb471f059

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,5 +24,8 @@ ODM_MANIFEST_GALAHAD_FILES := $(DEVICE_PATH)/manifest_galahad.xml
 # Kernel
 TARGET_KERNEL_CONFIG := lancelot_defconfig
 
+# Properties
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
+
 # Inherit the proprietary files
 include vendor/xiaomi/lancelot/BoardConfigVendor.mk
